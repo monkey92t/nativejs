739a9276e33c81925af3684a7cbec472b7e63d91
monkey92t <monkey92t@gmail.com>
2018-08-13 10:28:06 +0800

    Update call_billing_test.js
    add game names

--- call_billing_test.js
+++ call_billing_test.js
@@ -9,11 +9,13 @@ var host = "http://qa.megatron.baidu-shucheng.com:8091/game_billing?";
 //var host = "https://megatron.xmkanshu.com/game_billing?";
 
 var gameid = "test_game_id";
+var gamename = "游戏名称"
 var orderid = "orderid";
 var ordername = "订单商品信息";
 var ordermoney = "50000";
 
 var url = host + "gameid=" + encodeURIComponent(gameid) +
+    "&gamename=" + encodeURIComponent(gamename) +
     "&orderid=" + encodeURIComponent(orderid) +
     "&ordername=" + encodeURIComponent(ordername) +
     "&ordermoney=" + encodeURIComponent(ordermoney);
