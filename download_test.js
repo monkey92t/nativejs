039a33cc3115d18a8209447635e669de500d251f
monkey92t <monkey92t@gmail.com>
2020-08-26 14:50:20 +0800

    update func

--- download_test.js
+++ download_test.js
@@ -1,5 +1,5 @@
 //url是要下载的apk地址，adid联系我方获取
-var callBilling = function(url, adid) {
+var call = function(url, adid) {
     var p = "pandareader://action/addown?url=" + encodeURIComponent(url) + "&adid=" + adid
     $.WX.callProtocol(p);
 }
